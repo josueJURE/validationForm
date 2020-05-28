bb213ac237719b7979fa83507dbf0aa68995190b
josueJURE <josue.jure@gmail.com>
2020-05-28 19:40:19 +0100

    add checkInputs function

--- index.js
+++ index.js
@@ -8,5 +8,27 @@ const password2 = document.getElementById("password2");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  
-})
+
+  checkInputs();
+
+});
+
+function checkInputs() {
+
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if(usernameValue === "") {
+    // show error
+    // add error class
+    setErrorFor(username, 'username cannot be blank');
+
+  } else {
+    // add sucess classe
+    setSuccesFor(username)
+  }
+
+
+}
